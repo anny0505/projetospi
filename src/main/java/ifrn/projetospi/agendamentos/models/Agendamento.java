a63b707b230c1c611f63e2a58fd618fab48f4d51
Anny Freire <anny.yasmim@escolar.ifrn.edu.br>
2024-12-15 13:46:05 -0300

    Configurando modelo de agendamentos

--- src/main/java/ifrn/projetospi/agendamentos/models/Agendamento.java
+++ src/main/java/ifrn/projetospi/agendamentos/models/Agendamento.java
@@ -1,12 +1,27 @@
 package ifrn.projetospi.agendamentos.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Agendamento {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String data;
 	private String hora;
 	private String local;
 	private String evento;
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getData() {
 		return data;
 	}
